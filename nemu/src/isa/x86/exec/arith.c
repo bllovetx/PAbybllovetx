469450270875492e3677545777c464411134f5f1
tracer-ics2019 <tracer@njuics.org>
2019-10-21 23:29:18 +0800

    >  compile 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:29:18 up 1 day,  4:45, 16 users,  load average: 1.02, 0.67, 0.53 9ca65bbe659f8f65c2b4372ad2f5c9a6e73e6f

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -77,13 +77,12 @@ make_EHelper(adc) {
   rtl_update_ZFSF(&s1, id_dest->width);
 
   // update CF
-  rtl_is_add_carry(&s1, &s1, &s0);
+  rtl_is_add_carry(&s2, &s1, &s0);
   rtl_is_add_carry(&s0, &s0, &id_dest->val);
-  rtl_or(&s0, &s0, &s1);
+  rtl_or(&s0, &s0, &s2);
   rtl_set_CF(&s0);
 
   // update OF
-  assert(0);
   rtl_is_add_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
 
@@ -112,6 +111,7 @@ make_EHelper(sbb) {
   rtl_set_CF(&s0);
 
   // update OF
+  assert(0);
   rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
 
