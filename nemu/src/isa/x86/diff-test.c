f103f4447a9d24b75666b692ebb3b6d70d0294cb
tracer-ics2019 <tracer@njuics.org>
2019-10-24 09:34:18 +0800

    >  compile 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:34:18 up  1:19, 13 users,  load average: 0.14, 0.24, 0.20 26b080de6cd640eca0362604ee6a46aeecb82528

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,13 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool testflag=1;
+  for(int i=0;i<8;i++){
+	  if(ref_r->gpr[i]._32!=cpu.gpr[i]._32)testflag=0;
+  }
+  if(ref_r->pc!=cpu.pc)testflag=0;
+  if(ref_r->EFLAGS!=cpu.EFLAGS)testflag=0;
+  return testflag;
 }
 
 void isa_difftest_attach(void) {
