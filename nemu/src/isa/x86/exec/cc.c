282d9f1a225339c2dd0326d0ff26832c8a0570c9
tracer-ics2019 <tracer@njuics.org>
2019-10-29 22:22:32 +0800

    >  compile 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:22:32 up  5:13,  5 users,  load average: 1.22, 1.47, 1.60 eab1971e961df144db499b7f2571eb59b2539a71

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -15,7 +15,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:TODO();
-    case CC_B:TODO();
+  case CC_B: *dest = ( (cpu.eflags.CF==1) ? 1 : 0 );break;
 	case CC_E: *dest = ( (cpu.eflags.ZF==1) ? 1 : 0 );break;
 	case CC_BE:*dest = ( ((cpu.eflags.CF==1)||(cpu.eflags.ZF==1)) ? 1 : 0 );break;
 	case CC_S: *dest = ( (cpu.eflags.SF==1) ? 1 : 0 );break;
