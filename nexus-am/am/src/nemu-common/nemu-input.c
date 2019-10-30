e8b7fee821d91471308322d51048fd33ee3b467c
tracer-ics2019 <tracer@njuics.org>
2019-10-30 14:58:17 +0800

    >  run 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:58:17 up 21:48, 11 users,  load average: 2.29, 1.99, 1.80 d496419f62de5539a671f77c7245f82d3b6ed17

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -3,13 +3,15 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+static uint32_t key_scancode;
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      key_scancode = inl(KBD_ADDR);
+      kbd->keydown = (key_scancode & KEYDOWN_MASK) ? 1 : 0 ;
+      kbd->keycode = (key_scancode & (~KEYDOWN_MASK));
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
