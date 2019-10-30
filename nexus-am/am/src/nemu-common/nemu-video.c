28f78c92634e2511b0058b8ed5b5b3a2b491920e
tracer-ics2019 <tracer@njuics.org>
2019-10-30 19:09:09 +0800

    >  run 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:09:09 up 1 day,  1:59, 11 users,  load average: 1.96, 2.17, 2.31 42fd84155b7f6a534db23f80b2488bede1fa27d6

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -46,13 +46,13 @@ void __am_vga_init() {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   int choose, colorinit = 0xa61b29;
   for (int i = 0; i < size; i ++) {
-    switch((i/(SCREEN_W*30)%3)) {
-      case 0: choose = 0 ;break;
+    switch((i/SCREEN_W)%3) {
+      case 0: choose = 1 ;break;
       case 1: choose = 1 << 8 ;break;
       case 2: choose = 1 << 16;break;
     }
     fb[i] = colorinit;
-    colorinit +=choose;
+    if((i/SCREEN_W)%10==0) colorinit +=choose;
   } 
   draw_sync();
 }
