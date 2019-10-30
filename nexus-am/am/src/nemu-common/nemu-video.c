970890bfb7f63d8c6bf71f5a62a1203c8adcc1eb
tracer-ics2019 <tracer@njuics.org>
2019-10-30 17:11:53 +0800

    >  run 181240004 bllovetx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:11:53 up 1 day, 2 min, 11 users,  load average: 3.06, 2.98, 2.47 2fa0dafd9673d9fbca4d81d358ff086e583107a

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,13 +1,17 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
+#define SCREEN_H 300
+#define SCREEN_W 400
+static uint32_t video_readsize;
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      video_readsize = inl(SCREEN_ADDR);      
+      info->width = video_readsize >> 16;
+      info->height = video_readsize & 0xffff;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,7 +22,13 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      for(int i=0;i<ctl->h;i++){
+        //assert(ctl->y+i<SCREEN_H);
+        for(int j=0;j<ctl->w;j++){
+          //assert(ctl->x+j<SCREEN_W);
+          outl(FB_ADDR+SCREEN_W*(ctl->y+i)+ctl->x+j,*ctl->pixels);
+        }
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
